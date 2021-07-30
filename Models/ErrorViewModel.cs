76ee2f9322e719f9a2725b6034862d65c60b4450
velotist <velospam@posteo.de>
2021-07-30 20:56:07 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace UdemyTimGulestineDependencyInjection.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
