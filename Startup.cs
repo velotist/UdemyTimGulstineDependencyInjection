e9aa5a34b6111e3d53874b0a71bb9261d3327920
velotist <velospam@posteo.de>
2021-07-31 09:10:06 +0200

    Cleanup code

--- Startup.cs
+++ Startup.cs
@@ -1,14 +1,9 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace UdemyTimGulestineDependencyInjection
 {
@@ -30,7 +25,6 @@ namespace UdemyTimGulestineDependencyInjection
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
-
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
